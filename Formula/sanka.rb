9a2418ab4626486a0d1579e0383c118c1822f027
Haegwan Kim <haegwankim@gmail.com>
2026-09-05 11:16:29 +0900

    Declare Linux build tools for keyring dependencies

--- Formula/sanka.rb
+++ Formula/sanka.rb
@@ -11,6 +11,9 @@ class Sanka < Formula
   depends_on "python@3.12"
 
   on_linux do
+    depends_on "pkgconf" => :build
+    depends_on "rust" => :build
+
     resource "cffi" do
       url "https://files.pythonhosted.org/packages/eb/56/b1ba7935a17738ae8453301356628e8147c79dbb825bcbc73dc7401f9846/cffi-2.0.0.tar.gz"
       sha256 "44d1b5909021139fe36001ae048dbdde8214afa20200eda0f64c068cac5d5529"
